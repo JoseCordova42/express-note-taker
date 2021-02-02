d6dc7a0f55ce198cd4f7aca38231fa9573475e86
Jose <jose42cordova@gmail.com>
2021-02-01 20:13:07 -0600

    installed uuid, can now view and delete notes, but can only write if fresh on page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,36 @@ const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 const dataBase = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
     res.json(dataBase);
 });
 
 router.post('/notes', (req, res) => {
-    dataBase.push(req.body);
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    dataBase.push(newNote);
 
-    fs.writeFile('./db/db.json', JSON.stringify(dataBase, null, '\t'), () => {
+    fs.writeFile('./db/db.json', JSON.stringify(dataBase, null, '\t'), (err) => {
+        if (err) throw err;
         res.json(dataBase);
-    });
+    });    
     
 });
 
-// router.delete('/notes', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
+    for (let i =0; i < dataBase.length; i++) {
+        if (dataBase[i].id === req.params.id) {
+            dataBase.splice(i, 1);
+        }
+    };
 
-// });
+    fs.writeFile('./db/db.json', JSON.stringify(dataBase, null, '\t'), (err) => {
+        if (err) throw err;
+        res.json(dataBase);
+    });    
+
+});
 
 module.exports = router;
